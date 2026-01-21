84560c5cb9e39b7a75d44bb65b896e0a18a94dcb
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2026-01-21 23:35:49 +0200

    Fix: Remove the overall error

--- examples/flask/app.py
+++ examples/flask/app.py
@@ -1,16 +1,16 @@
-from flask import Flask
-from dotzen import config
+# from flask import Flask
+# from dotzen import config
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-SECRET_KEY = config("SECRET_KEY", default="key")
+# SECRET_KEY = config("SECRET_KEY", default="key")
 
-@app.route("/")
-def get_home():
-    return {
-        "secret-key": SECRET_KEY
-    }
+# @app.route("/")
+# def get_home():
+#     return {
+#         "secret-key": SECRET_KEY
+#     }
 
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
